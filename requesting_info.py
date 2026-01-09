7ed0102017edcddfa56707a5c936c0f270a74f81
Austin Singer <auslinux4@gmail.com>
2026-01-08 20:56:59 -0500

    Create requesting_info.py

---
+++ requesting_info.py
@@ -0,0 +1,14 @@
+# Requesting data
+
+name = input("What's your name?\n")
+
+hello = "Hello"
+
+print(hello + " " + name + "!")
+
+age = float(input("How old are you?\n"))
+
+decades = age/10
+years = age % 10
+
+print("You are " + str(decades) + " decades and " + str(years) + " years old.")
